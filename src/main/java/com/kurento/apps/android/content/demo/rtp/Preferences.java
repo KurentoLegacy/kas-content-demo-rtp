2c2f8d4e6b543a90a979f66a0433414bcd2c1f48
Miguel París Díaz <mparisdiaz@gmail.com>
2013-09-17 17:36:53 +0200

    Extend demos list to add custom URLs
    Change-Id: Ie1066e8637c367bc068777b555798bf6c18977e9

--- src/main/java/com/kurento/apps/android/content/demo/rtp/Preferences.java
+++ src/main/java/com/kurento/apps/android/content/demo/rtp/Preferences.java
@@ -1,18 +1,29 @@
 package com.kurento.apps.android.content.demo.rtp;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.EditTextPreference;
 import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
 import android.text.InputType;
+import android.widget.EditText;
 
 public class Preferences extends PreferenceActivity {
 
@@ -23,6 +34,7 @@ public class Preferences extends PreferenceActivity {
 	public static final String SERVER_ADDRESS_KEY = "SERVER_ADDRESS_KEY";
 	public static final String SERVER_PORT_KEY = "SERVER_PORT_KEY";
 	public static final String DEMOS_LIST_KEY = "DEMOS_LIST_KEY";
+	public static final String CUSTOM_DEMOS_SET_KEY = "CUSTOM_DEMOS_SET_KEY";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -66,21 +78,122 @@ public class Preferences extends PreferenceActivity {
 				.setDefaultValue(getString(R.string.preference_server_port_default));
 		root.addPreference(serverPortText);
 
-		// Demo selector
-		ListPreference demosList = new ListPreference(this);
-		demosList.setKey(DEMOS_LIST_KEY);
-		demosList.setTitle(getText(R.string.preference_demo_selector));
-		demosList
-				.setSummary(getText(R.string.preference_demo_selector_summary));
-		demosList.setEntries(getResources().getStringArray(
-				R.array.preference_demos_list));
-		demosList.setEntryValues(getResources().getStringArray(
-				R.array.urls_list));
-		demosList
-				.setDefaultValue(getString(R.string.preference_url_demo_default));
-		root.addPreference(demosList);
+		root.addPreference(buildDemoSelector());
 
 		return root;
+	}
+
+	private ListPreference buildDemoSelector() {
+		final ListPreference demosList = new ListPreference(this);
+		demosList.setKey(DEMOS_LIST_KEY);
+		demosList.setTitle(getText(R.string.preference_demo_selector));
+		demosList.setSummary(getDemoUrl(this));
+
+		CharSequence[] demos = getDemos();
+		demosList.setEntries(demos);
+		demosList.setEntryValues(demos);
+		demosList
+				.setDefaultValue(getString(R.string.preference_url_demo_default));
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle(getString(R.string.preference_custom_demo_url_title));
+
+		final EditText input = new EditText(this);
+		input.setInputType(InputType.TYPE_CLASS_TEXT);
+		builder.setView(input);
+
+		builder.setPositiveButton(android.R.string.ok,
+				new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						String t = input.getText().toString();
+						if (!t.isEmpty()) {
+							demosList.setSummary(t);
+							CharSequence[] newEntries = Preferences.this
+									.updateCustomDemos(t);
+							demosList.setEntries(newEntries);
+							demosList.setEntryValues(newEntries);
+							demosList.setValue(t);
+						}
+					}
+				});
+
+		builder.setNegativeButton(android.R.string.cancel,
+				new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						dialog.cancel();
+					}
+				});
+
+		final AlertDialog dialog = builder.create();
+
+		demosList
+				.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+					@Override
+					public boolean onPreferenceChange(Preference preference,
+							Object newValue) {
+						if (getString(R.string.preference_custom_demo_item)
+								.equals(newValue)) {
+							dialog.show();
+							return false;
+						}
+
+						preference.setSummary(newValue.toString());
+						return true;
+					}
+				});
+
+		return demosList;
+	}
+
+	private CharSequence[] getDemos() {
+		CharSequence[] fixedDemos = getResources().getStringArray(
+				R.array.urls_list);
+		Set<String> customDemos = PreferenceManager
+				.getDefaultSharedPreferences(this).getStringSet(
+						CUSTOM_DEMOS_SET_KEY, null);
+
+		int entriesSize = fixedDemos.length + 1;
+		if (customDemos != null) {
+			entriesSize += customDemos.size();
+		}
+		CharSequence[] entries = new CharSequence[entriesSize];
+
+		int i = 0;
+		for (; i < fixedDemos.length; i++) {
+			entries[i] = fixedDemos[i];
+		}
+
+		if (customDemos != null) {
+			List<String> customDemosList = new ArrayList<String>(customDemos);
+			Collections.sort(customDemosList);
+			for (String demo : customDemosList) {
+				entries[i++] = demo;
+			}
+		}
+
+		entries[i++] = getString(R.string.preference_custom_demo_item);
+
+		return entries;
+	}
+
+	private void addCustomDemo(String entry) {
+		SharedPreferences pref = PreferenceManager
+				.getDefaultSharedPreferences(this);
+
+		Set<String> customDemos = pref.getStringSet(CUSTOM_DEMOS_SET_KEY, null);
+		if (customDemos == null) {
+			customDemos = new HashSet<String>();
+		}
+		customDemos.add(entry);
+
+		pref.edit().putStringSet(CUSTOM_DEMOS_SET_KEY, customDemos).commit();
+	}
+
+	CharSequence[] updateCustomDemos(String entry) {
+		addCustomDemo(entry);
+		return getDemos();
 	}
 
 	public static String getServerAddress(Context context) {
